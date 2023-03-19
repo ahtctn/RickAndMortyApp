03422e466f3d260cf837ad50518b1d425370da56
Ahmet Ali Çetin <ahtctn@gmail.com>
2023-03-19 17:37:18 +0300

    TabBarController logic done

--- RickAndMortyApp/Controllers/Core/RMTabBarController.swift
+++ RickAndMortyApp/Controllers/Core/RMTabBarController.swift
@@ -12,6 +12,43 @@ final class RMTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        setUpTabs()
+    }
+    
+    private func setUpTabs() {
+        let charactersVC = RMCharacterViewController()
+        let locationsVC = RMLocationViewController()
+        let episodesVC = RMEpisodeViewController()
+        let settingsVC = RMSettingsViewController()
+        
+        charactersVC.navigationItem.largeTitleDisplayMode = .automatic
+        locationsVC.navigationItem.largeTitleDisplayMode = .automatic
+        episodesVC.navigationItem.largeTitleDisplayMode = .automatic
+        settingsVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        let navCharacters = UINavigationController(rootViewController: charactersVC)
+        let navLocations = UINavigationController(rootViewController: locationsVC)
+        let navEpisodes = UINavigationController(rootViewController: episodesVC)
+        let navSettings = UINavigationController(rootViewController: settingsVC)
+        
+        navCharacters.tabBarItem = UITabBarItem(title: "Characters",
+                                                image: UIImage(systemName: "person"),
+                                                tag: 1)
+        navLocations.tabBarItem = UITabBarItem(title: "Locations",
+                                                image: UIImage(systemName: "globe"),
+                                                tag: 2)
+        navEpisodes.tabBarItem = UITabBarItem(title: "Episodes",
+                                                image: UIImage(systemName: "tv"),
+                                                tag: 3)
+        navSettings.tabBarItem = UITabBarItem(title: "Settings",
+                                                image: UIImage(systemName: "gear"),
+                                                tag: 4)
+        
+        for nav in [navSettings, navEpisodes, navLocations, navCharacters] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        
+        setViewControllers([navCharacters, navLocations, navEpisodes, navSettings], animated: true)
     }
 
 
